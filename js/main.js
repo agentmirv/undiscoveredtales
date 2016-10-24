65e21ba4d67e0ed1e1e6be2307d8bc4d798ac959
agentmirv <agentmirv@yahoo.com>
2016-10-24 06:51:23 -0400

    Updating to latest on master.

--- js/main.js
+++ js/main.js
@@ -216,15 +216,15 @@ Helper.getImage = function (imageKey) {
     return game.cache.getBitmapData(imageKey)
 }
 
-// Fisher–Yates Shuffle
+// Fisherï¿½Yates Shuffle
 // https://bost.ocks.org/mike/shuffle/
 Helper.shuffle = function (array) {
     var m = array.length, t, i;
 
-    // While there remain elements to shuffle…
+    // While there remain elements to shuffleï¿½
     while (m) {
 
-        // Pick a remaining element…
+        // Pick a remaining elementï¿½
         i = Math.floor(Math.random() * m--);
 
         // And swap it with the current element.
@@ -751,12 +751,14 @@ MapTileGroup.prototype.constructor = MapTileGroup;
 //=========================================================
 function MakeToken(game, id) {
     var tokenData = game.gamedata.mapTokens.find(function (item) { return item.id == id });
+    
     var tokenInstance = new TokenSprite(
         game,
         tokenData.x,
         tokenData.y,
         tokenData.imageKey,
         tokenData.clickId,
+        tokenData.clickConditions,
         tokenData.addToWorld);
 
     game.gamedataInstances.mapTokens[id] = tokenInstance;
@@ -781,14 +783,15 @@ function MakeDialog(game, id) {
 }
 
 //=========================================================
-function TokenSprite(game, x, y, imageKey, clickId, addToWorld) {
+function TokenSprite(game, x, y, imageKey, clickId, clickConditions, addToWorld) {
     Phaser.Sprite.call(this, game, x, y, Helper.getImage(imageKey));
 
     this.imageKey = imageKey;
     this.clickId = clickId;
+    this.clickConditions = clickConditions;
     this.addToWorld = addToWorld;
 
-    if (clickId != null) {
+    if (this.clickId != null || this.clickConditions != null) {
         this.inputEnabled = true;
         this.events.onInputUp.add(this.tokenClicked, this);
     }
@@ -798,19 +801,15 @@ TokenSprite.prototype = Object.create(Phaser.Sprite.prototype);
 TokenSprite.prototype.constructor = TokenSprite;
 
 TokenSprite.prototype.tokenClicked = function (token, pointer) {
+    // this == token?
     var movePlayer = new Phaser.Point()
     movePlayer.x = token.centerX + 300 - 20 - 48 //half message width - left margin - half image width
     movePlayer.y = token.centerY + game.presentationOffsetY
 
     // Check if the positions are equal first (perhaps the last click did the tween)
     if (movePlayer.equals(new Phaser.Point(Math.floor(player.body.x), Math.floor(player.body.y)))) {
-        var dialogInstance = MakeDialog(game, token.clickId)
-        // TODO add fadeIn()
-        game.add.tween(dialogInstance).from({ alpha: 0 }, 400, Phaser.Easing.Linear.None, true, 0, 0, false);
-        game.stage.addChild(dialogInstance)
-        console.log('equals')
+        TokenSprite.prototype.openDialog.call(token);
     } else {
-        console.log('not equals')
         var moveTween = game.add.tween(player.body).to({ x: movePlayer.x, y: movePlayer.y }, 1200, Phaser.Easing.Quadratic.Out, true, 0, 0, false);
 
         moveTween.onStart.addOnce(function () {
@@ -818,12 +817,29 @@ TokenSprite.prototype.tokenClicked = function (token, pointer) {
         })
 
         moveTween.onComplete.addOnce(function () {
-            var dialogInstance = MakeDialog(game, token.clickId)
-            // TODO add fadeIn()
-            game.add.tween(dialogInstance).from({ alpha: 0 }, 400, Phaser.Easing.Linear.None, true, 0, 0, false);
-            game.stage.addChild(dialogInstance)
+            TokenSprite.prototype.openDialog.call(token);
         })
     }
+}
+
+TokenSprite.prototype.openDialog = function() {
+    var clickId = null
+    if(this.clickConditions != null && Array.isArray((this.clickConditions))) {
+        for(var i = 0; i < this.clickConditions.length; i++){
+            var condition = this.clickConditions[i]
+            var globalVar = game.gamedata.globalVars.find(function (item){return item.id == condition.globalId})
+            if(globalVar.value == condition.value) {
+                clickId = condition.dialogId
+            }
+        }
+    } else if (this.clickId != null) {
+        clickId = this.clickId
+    }
+    
+    var dialogInstance = MakeDialog(game, clickId)
+    // TODO add fadeIn()
+    game.add.tween(dialogInstance).from({ alpha: 0 }, 400, Phaser.Easing.Linear.None, true, 0, 0, false);
+    game.stage.addChild(dialogInstance)
 }
 
 TokenSprite.prototype.fadeOut = function (callback) {
@@ -1191,6 +1207,9 @@ DialogGroup.prototype.buttonClicked = function (button, pointer) {
                     game.stage.addChild(dialogInstance)
                 }
                 restoreControl = false;
+            } else if (action.type == "setGlobal") {
+                var globalVar = game.gamedata.globalVars.find(function (item) { return item.id == action.globalId });
+                globalVar.value = action.value
             }
         }
     }
