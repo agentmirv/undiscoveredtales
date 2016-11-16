61ce05d85d1871fcaac045a72624525b25e4bf75
agentmirv <agentmirv@yahoo.com>
2016-11-15 20:44:55 -0500

    Added horror check confirm dialog.

--- app/js/main.js
+++ app/js/main.js
@@ -191,8 +191,8 @@ var GameState = {
         //=================================================
         // Game Start
         //=================================================
-        MakeRevealList(game, game.gamedata.playerStart.firstReveal)
-        //MakeMonster(game, "deep-one")
+        //MakeRevealList(game, game.gamedata.playerStart.firstReveal)
+        MakeMonster(game, "deep-one")
     },
 
     update: function () {
@@ -275,6 +275,69 @@ Helper.shuffle = function (array) {
 
 //TODO Polyfill for array.find?
 //TODO Polyfill for array.filter?
+
+//=========================================================
+function MakeHorrorCheckConfirmDialog(game) {
+    var horrorCheckConfirmDialogGroup = new HorrorCheckConfirmDialogGroup(game)
+    game.stage.addChild(horrorCheckConfirmDialogGroup)
+}
+
+function HorrorCheckConfirmDialogGroup(game) {
+    Phaser.Group.call(this, game);
+
+    // Modal
+    var modalBackground = game.make.sprite(game.stageViewRect.x, game.stageViewRect.y, 'pixelTransparent');
+    modalBackground.width = game.stageViewRect.width;
+    modalBackground.height = game.stageViewRect.height;
+    modalBackground.inputEnabled = true;
+    this.addChild(modalBackground);
+
+    var messageText = "Resolve a Horror check?"
+
+    // Message
+    var dialogMessage = new DialogMessage(game, messageText, null);
+    dialogMessage.alignIn(game.stageViewRect, Phaser.CENTER, 0, -game.presentationOffsetY)
+    this.addChild(dialogMessage);
+
+    var dialogCancel = new DialogButtonThin(game, "Cancel", 280);
+    dialogCancel.alignTo(dialogMessage, Phaser.BOTTOM_LEFT, -10, 10)
+    this.addChild(dialogCancel);
+
+    var dialogAction = new DialogButtonThin(game, "Confirm", 280);
+    dialogAction.alignTo(dialogMessage, Phaser.BOTTOM_RIGHT, -10, 10)
+    this.addChild(dialogAction);
+
+    var dialogCancelButton = game.make.sprite(dialogCancel.x, dialogCancel.y, 'pixelTransparent');
+    dialogCancelButton.width = dialogCancel.width;
+    dialogCancelButton.height = dialogCancel.height;
+    dialogCancelButton.inputEnabled = true;
+    dialogCancelButton.events.onInputUp.add(this.cancelClicked, this);
+    this.addChild(dialogCancelButton);
+
+    var dialogActionButton = game.make.sprite(dialogAction.x, dialogAction.y, 'pixelTransparent');
+    dialogActionButton.width = dialogAction.width;
+    dialogActionButton.height = dialogAction.height;
+    dialogActionButton.inputEnabled = true;
+    dialogActionButton.events.onInputUp.add(this.confirmClicked, this);
+    this.addChild(dialogActionButton);
+}
+
+HorrorCheckConfirmDialogGroup.prototype = Object.create(Phaser.Group.prototype);
+HorrorCheckConfirmDialogGroup.prototype.constructor = HorrorCheckConfirmDialogGroup;
+
+HorrorCheckConfirmDialogGroup.prototype.cancelClicked = function (button, pointer) {
+    var fadeOutTween = game.add.tween(this).to({ alpha: 0 }, 400, Phaser.Easing.Linear.None, true, 0, 0, false);
+    fadeOutTween.onComplete.addOnce(function () {
+        this.destroy(true);
+    }, this);
+}
+
+HorrorCheckConfirmDialogGroup.prototype.confirmClicked = function (button, pointer) {
+    var fadeOutTween = game.add.tween(this).to({ alpha: 0 }, 400, Phaser.Easing.Linear.None, true, 0, 0, false);
+    fadeOutTween.onComplete.addOnce(function () {
+        this.destroy(true);
+    }, this);
+}
 
 //=========================================================
 function MakeHorrorCheckDialog(game) {
@@ -518,6 +581,8 @@ Monster.prototype.monsterClicked = function () {
         } else {
             HudGroup.prototype.showMonsterDetail()
         }
+    } else if (game.hud.activeStep == "horrorCheck") {
+        MakeHorrorCheckConfirmDialog(game)
     }
 }
 
@@ -1022,6 +1087,7 @@ HudGroup.prototype.monsterAttack = function () {
 
 HudGroup.prototype.horrorCheck = function () {
     if (game.gamedataInstances.monsters.length > 0) {
+        game.hud.activeStep = "horrorCheck"
         HudGroup.prototype.hideMonsterDetail()
         HudGroup.prototype.hideMonsterTray()
         HudGroup.prototype.hideEnemyPhaseBG(function () {
