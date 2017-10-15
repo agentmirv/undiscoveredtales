1e78c14b14aa3432d9b2c466e1e4d2157d6d8590
Mirv <agentmirv@yahoo.com>
2017-10-15 15:06:25 +0000

    Finished up the random events.

--- app.1/js/hud/dialogs/randomevent.js
+++ app.1/js/hud/dialogs/randomevent.js
@@ -13,19 +13,36 @@ function RandomEventDialog(game, dialogData, dialogGroup) {
 
     var data = null;
 
+    var eventText = dialogData.text;
+
+    if (dialogData.hasOwnProperty("target") && dialogData.target == "investigator") {
+        // Get random investigator
+        var mathRandom = Math.random();
+        var randomIndex = Math.floor(mathRandom * game.gamedata.investigators.length);
+        var randomInvestigatorData = game.gamedata.investigators[randomIndex];
+
+        // Replace investigator name
+        eventText = eventText.replace(/<name>/g, randomInvestigatorData.name);
+
+        // Replace investigator pronoun
+        var reInvestigatorPronoun = new RegExp("<" + randomInvestigatorData.pronoun + ":(.+?)>", "g");
+        eventText = eventText.replace(reInvestigatorPronoun, "$1");
+
+        // Remove remaining pronoun
+        eventText = eventText.replace(/<.+?:.+?>/g, "");
+    }
+
     // Modal
     var modalBackground = new DialogModalBackground(game);
     this.addChild(modalBackground);
 
     // Message
-    var dialogMessage = new DialogMessage(game, dialogData.text, dialogData.imageKey);
+    var dialogMessage = new DialogMessage(game, eventText, dialogData.imageKey);
     dialogMessage.alignIn(game.stageViewRect, Phaser.CENTER, 0, -game.presentationOffsetY)
     this.addChild(dialogMessage);
 
     if (dialogData.type == "resolve") {
-        // Buttons for [No Effect] [Resolve Effect]
-        data = {};
-        data.dialogGroup = dialogGroup;
+        // Button for [No Effect]
         var dialogCancel = new DialogButtonThin(game, "No Effect", 280);
         dialogCancel.alignTo(dialogMessage, Phaser.BOTTOM_LEFT, -10, 10);
         dialogCancel.buttonInput.events.onInputUp.add(function () {
@@ -36,14 +53,16 @@ function RandomEventDialog(game, dialogData, dialogGroup) {
         }, this);
         this.addChild(dialogCancel);
     
-        data = {};
-        data.dialogGroup = dialogGroup;
+        // Button for [Resolve Effect]
         var dialogAction = new DialogButtonThin(game, "Resolve Effect", 280);
         dialogAction.alignTo(dialogMessage, Phaser.BOTTOM_RIGHT, -10, 10)
         dialogAction.buttonInput.events.onInputUp.add(function () {
             this.onClose.addOnce(function () {
-                var randomEventDialog = dialogGroup.dialogs.find(function (item) { return item.id == "resolve" });
-                MakeRandomEventDialog(this.game, randomEventDialog, dialogGroup);
+                if (dialogGroup.dialogs.length > 1) {
+                    MakeRandomEventDialog(this.game, dialogGroup.dialogs[1], dialogGroup);
+                } else {
+                    dialogGroup.doneSignal.dispatch();
+                }
             }, this);
             this.close();
         }, this);
@@ -51,8 +70,6 @@ function RandomEventDialog(game, dialogData, dialogGroup) {
         
     } else {
         // Button for [Continue]
-        data = {};
-        data.dialogGroup = dialogGroup;
         var dialogContinue = new DialogButtonThin(game, "Continue", 180);
         dialogContinue.alignTo(dialogMessage, Phaser.BOTTOM_CENTER, 0, 10)
         dialogContinue.buttonInput.events.onInputUp.add(function () {
