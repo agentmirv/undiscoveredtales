adfca90d4a236e77278bba600e4b8f60ba28fd6f
Mirv <agentmirv@yahoo.com>
2018-12-10 20:43:36 -0500

    Added cutsceneCamera condition to the player move methods.

--- src/player/PlayerSprite.js
+++ src/player/PlayerSprite.js
@@ -3,33 +3,37 @@ import Phaser from 'phaser'
 export default class PlayerSprite extends Phaser.GameObjects.Sprite {
     constructor (scene, x, y) {
         super(scene, x, y)
-        this.on('moveToMapToken', this.handleMoveToMapToken)
         this.cutsceneCamera = false
+        this.on('moveToMapToken', this.handleMoveToMapToken)
     }
 
     updateMoveCursor (cursors) {
-        const playerVelocity = 400
+        if (!this.cutsceneCamera) {
+            const playerVelocity = 400
         
-        this.body.setVelocity(0, 0)
-
-        if (cursors.up.isDown) {
-            this.body.setVelocityY(-playerVelocity)
-        }
-        else if (cursors.down.isDown) {
-            this.body.setVelocityY(playerVelocity)
-        }
-
-        if (cursors.left.isDown) {
-            this.body.setVelocityX(-playerVelocity)
-        }
-        else if (cursors.right.isDown) {
-            this.body.setVelocityX(playerVelocity)
+            this.body.setVelocity(0, 0)
+    
+            if (cursors.up.isDown) {
+                this.body.setVelocityY(-playerVelocity)
+            }
+            else if (cursors.down.isDown) {
+                this.body.setVelocityY(playerVelocity)
+            }
+    
+            if (cursors.left.isDown) {
+                this.body.setVelocityX(-playerVelocity)
+            }
+            else if (cursors.right.isDown) {
+                this.body.setVelocityX(playerVelocity)
+            }
         }
     }
 
     updateMoveDragPoint (dragPoint) {
-        this.x += dragPoint.x
-        this.y += dragPoint.y
+        if (!this.cutsceneCamera) {
+            this.x += dragPoint.x
+            this.y += dragPoint.y
+        }
     }
 
     handleMoveToMapToken (mapToken) {
